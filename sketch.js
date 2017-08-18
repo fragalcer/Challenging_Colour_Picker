d92a983775587ada56999e1caecaef93c1b975ef
Ben Swift <ben@benswift.me>
2017-08-18 13:23:16 +1000

    add extremely basic color-picker template

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+// this is the variable your picker should change
+var pickedColor = 0;
+
 function setup() {
-    createCanvas(800, 600);
-    // any additional setup code goes here
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    // your "draw loop" code goes here
+
+
+    // display the currently "picked" colour in the bottom-right
+    fill(pickedColor);
+    rect(width-100, height-100, 100, 100);
 }
