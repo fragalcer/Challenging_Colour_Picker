474b381cca3120d0d0c8998dd14fb974b919dc92
franciscogallardo <fragalcer@gmail.com>
2017-08-18 22:17:42 +1000

    TEST

--- sketch.js
+++ sketch.js
@@ -4,11 +4,12 @@ var pickedColor = 0;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
-
+//TEST
 function draw() {
 
 
     // display the currently "picked" colour in the bottom-right
     fill(pickedColor);
     rect(width-100, height-100, 100, 100);
+
 }
